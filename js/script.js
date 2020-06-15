9dd0be8a3d972fac2ee3db98d764dd8d27265991
Hugh Nelson <66570072+hughnelsonproductions@users.noreply.github.com>
2020-06-15 16:47:12 +0100

    movies

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// external js: masonry.pkgd.js
+
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true,
+  alert("Hello! I am an alert box!!");
+});
+
+alert("Hello! I am an alert box!!");
